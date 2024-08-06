f0f9f173dd8d3c95e396099ff6e4819fa792d164
Ayaansh Rajotia <86037577+ayaanshrajotia@users.noreply.github.com>
2024-08-06 17:12:00 +0530

    first commit

---
+++ streamlitDemo.py
@@ -0,0 +1,242 @@
+import streamlit as st
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+from io import BytesIO
+import tempfile
+
+
+def squats_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
+
+
+def pushup_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
+
+
+def squats_posture():
+    mp_drawing = mp.solutions.drawing_utils
+    mp_pose = mp.solutions.pose
+
+    cap = cv2.VideoCapture(0)
+    counter = 0
+    stage = None
+
+    with mp_pose.Pose(min_detection_confidence=0.5,
+                      min_tracking_confidence=0.5) as pose:
+
+        prev_frame_time = 0
+
+        # Streamlit video player
+        st.title('Squats')
+
+        # Create a placeholder for the video
+        frame_placeholder = st.empty()
+
+        flag = False
+
+        if st.button("Close"):
+            flag = True
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_CUBIC)
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+
+            results = pose.process(image)
+
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            try:
+                landmarks = results.pose_landmarks.landmark
+
+                hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+                       landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+                knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
+                        landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+                ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
+                         landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+
+                angle = squats_angle(hip, knee, ankle)
+
+                cv2.putText(image, str(angle),
+                            tuple(np.multiply(knee, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+
+                if angle > 160:
+                    stage = 'down'
+                if angle < 100 and stage == 'down':
+                    stage = 'up'
+                    counter += 1
+                    print(counter)
+            except:
+                pass
+
+            new_frame_time = time.time()
+            fps2 = 1 / (new_frame_time - prev_frame_time)
+            prev_frame_time = new_frame_time
+
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                      )
+
+            cv2.rectangle(image, (350, 25), (610, 75), (255, 174, 201), -1)
+            cv2.rectangle(image, (350, 25), (610, 75), (0, 0, 255))
+            cv2.putText(image, str(counter), (380, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            cv2.putText(image, 'REP', (440, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+            cv2.putText(image, "FPS : {0:.2f}".format(fps2), (490, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2,
+                        cv2.LINE_AA)
+            # Convert the frame to RGB format (OpenCV uses BGR by default)
+            frame_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+            # Display the frame in Streamlit
+            frame_placeholder.image(frame_rgb, channels='RGB', use_column_width=True)
+
+            if flag:
+                break
+
+        # Release the video capture object
+        cap.release()
+
+def pushup_posture():
+    mp_drawing = mp.solutions.drawing_utils
+    mp_pose = mp.solutions.pose
+
+    cap = cv2.VideoCapture(0)
+    counter = 0
+    stage = None
+
+    with mp_pose.Pose(min_detection_confidence=0.5,
+                      min_tracking_confidence=0.5) as pose:
+
+        prev_frame_time = 0
+
+        # Streamlit video player
+        st.title('Squats')
+
+        # Create a placeholder for the video
+        frame_placeholder = st.empty()
+
+        flag = False
+
+        if st.button("Close"):
+            flag = True
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_CUBIC)
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+
+            results = pose.process(image)
+
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            try:
+                landmarks = results.pose_landmarks.landmark
+
+                shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                            landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                         landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                         landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+
+                angle = pushup_angle(shoulder, elbow, wrist)
+
+                cv2.putText(image, str(angle),
+                            tuple(np.multiply(elbow, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+
+                if angle >= 160:
+                    stage = 'up'
+                if angle <= 90 and stage == 'up':
+                    stage = 'down'
+                    counter += 1
+                    print(counter)
+            except:
+                pass
+
+            new_frame_time = time.time()
+            fps = 1 / (new_frame_time - prev_frame_time)
+            prev_frame_time = new_frame_time
+
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                      mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
+
+            cv2.rectangle(image, (350, 25), (610, 75), (255, 174, 201), -1)
+            cv2.rectangle(image, (350, 25), (610, 75), (0, 0, 255))
+            cv2.putText(image, str(counter), (380, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            if stage:
+                cv2.putText(image, str(stage), (240, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            cv2.putText(image, 'REP', (440, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            cv2.putText(image, "FPS : {0:.2f}".format(fps), (490, 450), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2,
+                        cv2.LINE_AA)
+
+            # Convert the frame to RGB format (OpenCV uses BGR by default)
+            frame_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+            # Display the frame in Streamlit
+            frame_placeholder.image(frame_rgb, channels='RGB', use_column_width=True)
+
+            if flag:
+                break
+
+            # Release the video capture object
+        cap.release()
+
+
+def main():
+    st.title("Posture Corrector")
+
+    # Upload video file
+    # video_file = st.file_uploader("Upload a video", type=["mp4", "avi", "mov", "mkv"])
+    #
+    # if video_file is not None:
+    #     # Save the uploaded file to a temporary file
+    #     with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_file:
+    #         temp_file.write(video_file.read())
+    #         temp_file_path = temp_file.name
+
+    if st.button("Squats"):
+        squats_posture()  # Process and display the video with Mediapipe
+
+    if st.button("Pushups"):
+        pushup_posture()
+
+
+if __name__ == "__main__":
+    main()
